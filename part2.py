568f3b9578e769f2c4dd53df52a3e67cb9dc0281
leoncavalli <50718317+leoncavalli@users.noreply.github.com>
2020-07-26 17:48:41 +0300

    Part2:Creating a model to predict types of clothes.

---
+++ part2.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+
+fashion_mnist=keras.datasets.fashion_mnist
+(train_images,train_labels),(test_images,test_labels)=fashion_mnist.load_data()
+train_images = train_images/255.0
+test_images = test_images/255.0
+
+model=keras.Sequential([
+    keras.layers.Flatten(input_shape=(28,28)),
+    keras.layers.Dense(128,activation=tf.nn.relu),
+    keras.layers.Dense(10,activation=tf.nn.softmax)
+])
+model.compile(optimizer=tf.optimizers.Adam(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images,train_labels,epochs=10)
+
+print(model.evaluate(test_images, test_labels))
